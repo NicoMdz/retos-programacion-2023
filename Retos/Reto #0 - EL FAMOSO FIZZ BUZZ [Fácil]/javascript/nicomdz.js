aefbcfa70a723c316f27dbede0f6ad8e5b0dd958
Nicolás Méndez Brito <nick.mdz.b@gmail.com>
2023-10-17 14:02:55 -0600

    Reto 0

---
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/javascript/nicomdz.js
@@ -0,0 +1,16 @@
+function FizzBuzz() {
+    
+    for (let i = 0; i <= 100 ; i++) {
+        if (i % 15 == 0) {
+            console.log("FizzBuzz")
+        } else if (i % 3 == 0) {
+            console.log("Fizz")
+        } else if (1 % 5 == 0) {
+            console.log("Buzz")
+        } else {
+            console.log(i)
+        }
+    }  
+}
+
+FizzBuzz()
